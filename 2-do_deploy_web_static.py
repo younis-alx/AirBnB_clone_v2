ea119c1195ba9e07d7892a8ec7e130ece282145f
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-10-05 04:58:32 -0700

    manda task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,52 +1,41 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
-
+""" a Fabric script that distributes an archive to your web servers"""
 from fabric.api import *
-from os.path import exists
+from os import path
 
-env.user = 'ubuntu'
+
 env.hosts = ['100.26.235.153', '52.91.118.87']
-env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    """deploys the archives to server"""
+    try:
+        if not (path.exists(archive_path)):
+            return False
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        # uploading the archive
+        put(archive_path, "/tmp/")
+
+        # uncompress the archive
+        parts = archive_path.split('_')
+        time_stamp = parts[2].split(".")[0]
+        run("sudo rm -rf /data/web_static/releases/\
+web_static_{}".format(time_stamp))
+        run("sudo mkdir -p /data/web_static/releases/\
+web_static_{}".format(time_stamp))
+        run("sudo tar -xvf /tmp/web_static_{}.tgz -C /data/\
+web_static/releases/web_static_{}".format(time_stamp, time_stamp))
+
+        # delete the archive from web_server
+        run("sudo rm -rf /tmp/web_static_{}.tgz".format(time_stamp))
+
+        # delete the symbolic link
+        run("sudo rm -rf /data/web_static/current")
+
+        # create a new symblolic link
+        run("sudo ln -sf /data/web_static/releases/\
+web_static_{} /data/web_static/current".format(time_stamp))
+
+        return True
+    except Exception as e:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
