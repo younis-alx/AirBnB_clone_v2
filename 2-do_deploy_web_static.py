b8e179239919a63f5d0dac47d7f0d8d28d20f24a
jay-alx <126336909+younis-alx@users.noreply.github.com>
2023-10-08 09:20:32 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,21 +19,21 @@ def do_deploy(archive_path):
         # uncompress the archive
         parts = archive_path.split('_')
         time_stamp = parts[2].split(".")[0]
-        run("sudo rm -rf /data/web_static/releases/\
+        sudo("sudo rm -rf /data/web_static/releases/\
 web_static_{}".format(time_stamp))
-        run("sudo mkdir -p /data/web_static/releases/\
+        sudo("sudo mkdir -p /data/web_static/releases/\
 web_static_{}".format(time_stamp))
-        run("sudo tar -xvf /tmp/web_static_{}.tgz -C /data/\
+        sudo("sudo tar -xvf /tmp/web_static_{}.tgz -C /data/\
 web_static/releases/web_static_{}".format(time_stamp, time_stamp))
 
         # delete the archive from web_server
-        run("sudo rm -rf /tmp/web_static_{}.tgz".format(time_stamp))
+        sudo("sudo rm -rf /tmp/web_static_{}.tgz".format(time_stamp))
 
         # delete the symbolic link
-        run("sudo rm -rf /data/web_static/current")
+        sudo("sudo rm -rf /data/web_static/current")
 
         # create a new symblolic link
-        run("sudo ln -sf /data/web_static/releases/\
+        sudo("sudo ln -sf /data/web_static/releases/\
 web_static_{} /data/web_static/current".format(time_stamp))
 
         return True
